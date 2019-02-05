1ef84f1ba4c8764de956a99d195f79b30d82a0df
Mat Moore <matmoore@users.noreply.github.com>
2019-02-05 15:22:06 +0000

    Add models for offence and the law it references

--- sentencing_guidelines_api/api/models.py
+++ sentencing_guidelines_api/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class ReferencedActSection(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    act_name = models.CharField(max_length=100) # TODO: could we use a URL to identify the act?
+    section = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.act_name} - §{self.section}'
+
+class Offence(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    offense_name = models.CharField(max_length=100)
+    referenced_acts = models.ManyToManyField(ReferencedActSection, blank=True)
+    effective_from = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.offense_name
+
+admin.site.register(Offence)
+admin.site.register(ReferencedActSection)
