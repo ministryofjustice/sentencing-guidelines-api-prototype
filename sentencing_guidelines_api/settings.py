e6ed5669376b657f30c455f51e9149bafa7047d2
Mat Moore <matmoore@users.noreply.github.com>
2019-03-12 16:27:24 +0000

    Staticfiles
    Use example from https://devcenter.heroku.com/articles/django-assets
    
    (Currently without whitenoise)

--- sentencing_guidelines_api/settings.py
+++ sentencing_guidelines_api/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
